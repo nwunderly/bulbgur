ba0c65bb19d814db19c8b6db21399071f80af79e
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-11-07 15:11:22 -0500

    ports

--- run_server.sh
+++ run_server.sh
@@ -2,11 +2,11 @@
 
 if [ "$1" == "main" ]
 then
-  uvicorn --host 0.0.0.0 --port 10101 src.main:app
+  uvicorn --host 0.0.0.0 --port 9000 src.main:app
 
 elif [ "$1" == "static" ]
 then
-  uvicorn --host 0.0.0.0 --port 10102 src.static:app
+  uvicorn --host 0.0.0.0 --port 9001 src.static:app
 
 else
   exit 1
