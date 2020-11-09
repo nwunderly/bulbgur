2ea7a07d7eb9b7ba6fc290f90c9ef865a5093219
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-11-09 12:52:45 -0500

    add home and login templates

--- src/main.py
+++ src/main.py
@@ -1,18 +1,25 @@
 import asyncio
+import datetime
 import os
-import asyncpg
-
 from os.path import splitext
+
+import asyncpg
 from fastapi import FastAPI, HTTPException
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
-from starlette.responses import RedirectResponse, FileResponse
-from starlette.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 
-from auth import SECRET_KEY, POSTGRES_PASSWORD
+from auth import SECRET_KEY
 from src.db import Database
 
+
+#############
+# APP SETUP #
+#############
+
+
+started_at = datetime.datetime.now()
 
 app = FastAPI(redoc_url=None, docs_url=None)
 db = Database()
@@ -36,23 +43,72 @@ async def cleanup():
         await db.close()
 
 
-@app.get('/')
+#####################
+# USER-FACING PATHS #
+#####################
+
+
+@app.route('/')
 async def index(request: Request):
-    return "bulbe.rocks image API is active."
+    return templates.TemplateResponse('index.html', {'request': request})
 
 
-@app.post('/shorten')
-async def create_short_url(request: Request):
+@app.route('/status')
+async def server_status(request: Request):
+    return f"API is online. Runtime {datetime.datetime.now() - started_at}"
+
+
+@app.get('/login')
+async def login_screen(request: Request):
+    return templates.TemplateResponse('login.html', {'request': request})
+
+
+@app.post('/authenticate')
+async def authenticate_user(request: Request):
+    # TODO: this
+    return "NOT YET IMPLEMENTED"
+
+
+#######################
+# API : URL SHORTENER #
+#######################
+
+
+@app.post('/short_url/new/')
+async def new_short_url(request: Request):
     # TODO: This
     return "NOT YET IMPLEMENTED"
 
 
-@app.get('/delete/{short_code:path}')
-async def delete_short_url(request: Request, short_code: str = None):
+@app.route('/short_url/del/{short_code}', methods=['GET', 'DELETE'])
+async def del_short_url(request: Request, short_code: str = None):
+    # TODO: This
     return "NOT YET IMPLEMENTED"
 
 
-@app.get('/{short_code:path}')
+######################
+# API : IMAGE SERVER #
+######################
+
+
+@app.post('/file_upload/new/')
+async def new_file_upload(request: Request):
+    # TODO: This
+    return "NOT YET IMPLEMENTED"
+
+
+@app.route('/file_upload/del/{image_name}', methods=['GET', 'DELETE'])
+async def del_file_upload(request: Request, image_name: str = None):
+    # TODO: This
+    return "NOT YET IMPLEMENTED"
+
+
+#####################################
+# CATCH-ALL : HANDLE SHORTENED URLS #
+#####################################
+
+
+@app.get('/{short_code}')
 async def redirect(request: Request, short_code: str = None):
     if not short_code or len(short_code) > 10:
         raise HTTPException(status_code=404)
@@ -61,6 +117,10 @@ async def redirect(request: Request, short_code: str = None):
         raise HTTPException(status_code=404)
     return RedirectResponse(long_url, status_code=301)
 
+
+###################################################
+# MIDDLEWARE FOR HANDLING LOGIN SESSIONS SECURELY #
+###################################################
 
 
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY, same_site="strict", https_only=True, max_age=14*24*60*60)
--- src/static.py
+++ src/static.py
@@ -6,5 +6,6 @@ app.token = None
 image_folder = 'data/'
 
 app.mount('/css', StaticFiles(directory='css'), name='css')
-app.mount('/image', StaticFiles(directory='data'), name='images_legacy_support')
+app.mount('/assets', StaticFiles(directory='assets'), name='site_assets')
+# app.mount('/image', StaticFiles(directory='data'), name='images_legacy_support')
 app.mount('/', StaticFiles(directory='data'), name='images')
