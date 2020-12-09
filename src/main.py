e3e4234a1a48fb500318d836df83c14bf8bc9f54
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-12-09 15:38:18 -0500

    return just complete short url

--- src/main.py
+++ src/main.py
@@ -126,9 +126,9 @@ async def dash(request: Request):
 async def new_short_url(request: Request):
     if request.headers.get('x-authorization') == API_KEY or request.query_params.get('api_key') == API_KEY:
         long_url = request.query_params['long_url']
-        short_code = request.query_params.get('short_code') or secrets.token_urlsafe(5)
+        short_code = request.query_params.get('short_code')
         short_code = await db.new_short_url(long_url, short_code)
-        return {'short_code': short_code, 'long_url': long_url}
+        return PlainTextResponse("https://bulbe.rocks/"+short_code)
     else:
         raise HTTPException(status_code=401)
 
